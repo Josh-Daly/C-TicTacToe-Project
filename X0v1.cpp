ff0349dc187b1c34fdf2c97c846e7f185541ee4e
Paul Daly <g00470372@atu.ie>
2025-03-27 16:19:25 +0000

    Initial File

---
+++ X0v1.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <stdlib.h> 
+using namespace std;
+
+int main()
+{
+	int state = 1, i, j;
+	//State: 1 is setup, 2 is playing, 0 is game exited
+	char ans = 'n';
+	const int BOARDSIZE = 5;
+	char gameBoard[BOARDSIZE][BOARDSIZE];
+
+
+
+	while (state != 0)
+	{
+
+		//Loop to setup board, called at start and after reset, called by setting state to 1
+		if (state == 1)
+		{
+			gameBoard[0][0] = ' ';
+			gameBoard[0][1] = '|';
+			gameBoard[0][2] = ' ';
+			gameBoard[0][3] = '|';
+			gameBoard[0][4] = ' ';
+			// Above = [] | [] | []
+			gameBoard[1][0] = '-';
+			gameBoard[1][1] = '+';
+			gameBoard[1][2] = '-';
+			gameBoard[1][3] = '+';
+			gameBoard[1][4] = '-';
+			// Above = -+-+-
+			gameBoard[2][0] = ' ';
+			gameBoard[2][1] = '|';
+			gameBoard[2][2] = ' ';
+			gameBoard[2][3] = '|';
+			gameBoard[2][4] = ' ';
+			// Above = []|[]|[]
+			gameBoard[3][0] = '-';
+			gameBoard[3][1] = '+';
+			gameBoard[3][2] = '-';
+			gameBoard[3][3] = '+';
+			gameBoard[3][4] = '-';
+			// Above = -+-+-
+			gameBoard[2][0] = ' ';
+			gameBoard[2][1] = '|';
+			gameBoard[2][2] = ' ';
+			gameBoard[2][3] = '|';
+			gameBoard[2][4] = ' ';
+			// Above = []|[]|[]
+			gameBoard[3][0] = '-';
+			gameBoard[3][1] = '+';
+			gameBoard[3][2] = '-';
+			gameBoard[3][3] = '+';
+			gameBoard[3][4] = '-';
+			// Above = -+-+-
+			gameBoard[4][0] = ' ';
+			gameBoard[4][1] = '|';
+			gameBoard[4][2] = ' ';
+			gameBoard[4][3] = '|';
+			gameBoard[4][4] = ' ';
+			// Above = []|[]|[]
+
+			state = 2;
+		}
+
+		if (state == 2)
+		{
+			//Print Board
+			for (i = 0; i < BOARDSIZE; i++)
+			{
+				for (j = 0; j < BOARDSIZE; j++)
+				{
+					cout << gameBoard[i][j];
+				}
+				cout << endl;
+			}
+
+			//Loop to check for victory conditions
+			for (i = 0; i < BOARDSIZE; i++)
+			{
+
+			}
+
+
+
+
+
+
+			cout << "Resume?" << endl;
+			cin >> ans;
+			system("cls");
+			if (ans != 'y' && ans != 'Y')
+			{
+				state = 0;
+			}
+
+		}
+
+
+
+		system("cls");
+	}
+
+	//clear screen command
+	system("cls");
+	cout << "Game Function terminated." << endl;
+
+	return 0;
+}
+
